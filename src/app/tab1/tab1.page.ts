bf794881b3499402aaa1591a459cb09627aa4cb6
ADELEKE Kismath <adeleke@localhost.localdomain>
2020-02-09 19:38:44 +0100

    Mise-a-jour

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,13 +8,15 @@ import { isNumber } from 'util';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  nvEntree = true;
+ //Variable  pour verifier si la virgule n'est pas placé deux fois dans le meme nombre
+   UneVirgule = false;
+   plusoumoins = false;
 
   // Variable contenant le calcul
   Calculate = '0';
 
   // tableau contenant la liste des valeurs à calculer 
-  list: number[] = [] ;
+  list = [] ;
 
   //variable contenant la position des operateurs
   IndexOperator = null;
@@ -28,11 +30,11 @@ export class Tab1Page {
   readyForAnother = false; 
   // Declaration d'un tableau contenant les differents symboles de la calculatrice
   numberGroups = [
-    ['.' , 'Sup' , 'sqrt' , '%'],
+    ['AC' ,'+/-', '%', '÷'],
     [7 , 8 , 9 , 'x'],
     [4 , 5 , 6 , '-'],
     [1 , 2 , 3 , '+'],
-    [0 , '/' , '=', 'AC'],
+    [0 , 'DEL' ,'.', '=', ],
 
   ];
 
@@ -42,24 +44,14 @@ export class Tab1Page {
      // Lorsqu'il n'y a pas encore d'operateur
        if (this.Operator === '') {
         this.Operator = signe;
-        // Le tableau recoit le premier element à calculer
-        this.list.push(Number(this.Calculate));
         this.Calculate += '' + signe; 
         this.readyForAnother = false;
-        this.IndexOperator = 0;
-        console.log(this.list);
       } else {
         //Dans le cas contrait
         if (signe === this.Operator) {
-          this.IndexOperator += 1;
           this.Calculate += '' + signe; 
-           // Le tableau recoit l'element precedent le signe de calcul
-          let nbr:number = Number(this.Calculate.substring(this.Calculate.indexOf(signe, this.IndexOperator),
-          this.Calculate.lastIndexOf(signe)));
-          nbr = Math.abs(nbr) ;
-          this.list.push(nbr);
           this.readyForAnother = false;
-          console.log(this.list);
+        
        }}
     }
   }
@@ -67,191 +59,176 @@ export class Tab1Page {
   Calculer() {
     // Verifie si le dernier element de l'operation n'est pas un signe
     if (Number(this.Calculate.substring((this.Calculate.lastIndexOf(this.Operator) + 1), this.Calculate.length))) {
-    this.list.push(Number(this.Calculate.substring((this.Calculate.lastIndexOf(this.Operator) + 1), this.Calculate.length)));
-    console.log(this.list);
+    this.list = this.Calculate.split(this.Operator);
+    this.list.forEach(element => {
+      
+    });
+      console.log(this.list);
     //L'operation d'addition
     if (this.Operator === '+' ) {
-     let somme: number = 0;
-     this.list.forEach( function(value) {
-       somme += value;
-      });
-     this.Calculate = '' + somme;
-     this.list = [];
-     this.Operator = '';
+      this.addition();
      }
-
      //L'operation de soustraction
     else if (this.Operator === '-' ) {
-      //console.log()
-      let moins: number;
-      this.list.forEach( function(value, index, array) {
-        if (index === 0 ) {
-           moins = value;
-           console.log(value);
-        }else{
-          console.log(value);
-          moins -=  value;
-
-        }
-       });
-      this.Calculate = '' +  moins;
-      this.list = [];
-      this.Operator = '';
+      this.Difference();
       }
-
-      //L'operation de multiplication
-     else if (this.Operator === 'x' ) {
-        let fois: number = 1;
-        this.list.forEach( function(value) {
-          fois *= value;
-         });
-        this.Calculate = '' + fois;
-        this.list = [];
-        this.Operator = '';
+     //L'operation de multiplication
+    else if (this.Operator === 'x' ) {
+          this.Produit();
         }  
-        
-        //L'operation de division
-        else if (this.Operator === '/' ) {
-          let div: number;
-          this.list.forEach( function(value, index, array) {
-            if (index === 0 ) {
-              div = value;
-            }else{
-              if (value === 0) {
-                this.Calculate = 'Error';
-                this.valueInit = true;
-                this.list = [];
-                this.Operator = '';
-              } else {
-                div /= value;
-              }
-             
-            }
-          });
-          this.Calculate = '' + div;
-          this.list = [];
-          this.Operator = '';
+    //L'operation de division
+    else if (this.Operator === '÷' ) {
+          this.Divison();
           }  
-
-          else if (this.Operator === '%' ) {
-            let div: number;
-            this.list.forEach( function(value, index, array) {
-              if (index === 0 ) {
-                div = value;
-              }else{
-                if (value === 0) {
-                  this.Calculate = 'Error';
-                  this.valueInit = true;
-                  this.list = [];
-                  this.Operator = '';
-                } else {
-                  div %= value;
-                }
-               
-              }
-            });
-            this.Calculate = '' + div;
-            this.list = [];
-            this.Operator = '';
-            }    
+    else if (this.Operator === '%' ) {
+        this.Modulo(); 
     }
+   
+  }
      else{
-       
-      this.Calculate = 'Error';
-      this.valueInit = true;
-      this.list = [];
-      this.Operator = '';
+   this.Erreur();
   }}
   // Fonction declencher lors du clic
   onClick(symbol) {
     console.log(symbol);
-
+   //Si le symbole sur lequel l'utilisateur a cliqué est un chiffre
     if (isNumber(symbol)) {
       if (this.valueInit === true) {
         this.Calculate = '' + symbol; 
         this.valueInit = false;
-        this.readyForAnother = true;
       } else { 
       this.Calculate += '' + symbol; 
-      this.readyForAnother = true;
        }
+       this.UneVirgule = false;
+       this.readyForAnother = true;
+    }else{
+      switch (symbol) {
+        case 'AC':
+           // La fonction pour efacer l'ecran
+          this.Calculate = '0';
+          this.valueInit = true;
+          break;
 
+        case 'DEL':
+          //La fonction qui permet de supprimer la derniere entree 
+         this.Supprimer1();
+          break; 
+         
+        case '.':
+          //Fonction permettant d'ajouter une virgule 
+          if (this.readyForAnother && !this.UneVirgule) {
+            this.Calculate += '.';
+            this.UneVirgule = true;
+          } 
+          break;
+
+        case '+/-':
+           /*  //Fonction permettant d'ajouter le plus ou moins
+          if (!this.readyForAnother) {
+            this.Calculate += '(-';
+          }  */
+            break;  
+  
+        case '=':
+          // L'egalité
+          console.log(!isNumber(this.Calculate));
+          if (!isNumber(this.Calculate)) { 
+               this.Calculer();
+          } 
+          break;   
+        default:
+          this.signeCalcul(symbol);
+          break;
+      }
     }
-    // La fonction pour efacer l'ecran
-    else if(symbol === 'AC'){
-      this.Calculate = '0';
-      this.valueInit = true;
-
-    }
-
-    // La fonction racine carré
-    else if(symbol === 'sqrt'){
-            let racine: number ;
-            
-            if (this.Operator === '') {
-               racine =  Math.sqrt(Number(this.Calculate));
-               console.log(racine);
-               this.Calculate = '' + racine;
-               this.list = [];
-               this.Operator = '';
-             } else{
-              this.Calculate = 'Error';
-              this.valueInit = true;
-              this.list = [];
-              this.Operator = '';
-          }
-
-    }
-
-     // La fonction x au carré
-    else if(symbol === 'x^2'){
-      let racine: number ;
-     
-      if (this.Operator === '') {
-         racine =  Math.pow(Number(this.Calculate), 2);
-         console.log(racine);
-         this.Calculate = '' + racine;
-         this.list = [];
-         this.Operator = '';
-       } else{
-        this.Calculate = 'Error';
-        this.valueInit = true;
-        this.list = [];
-        this.Operator = '';
-    }
-
-}   
- // La fonction pour efacer la derniere entree de l'ecran
-    else if(symbol === 'Sup') {
-      if (this.Calculate.length === 1) {
-        this.Calculate = '0';
-        this.valueInit = true;
-      } else {
-        this.Calculate = this.Calculate.substring(0, (this.Calculate.length - 1));
-        if (isNumber(this.Calculate.substring( (this.Calculate.length - 1), this.Calculate.length))) {
-          this.readyForAnother = true;
+}
+  addition(){
+    let somme: number = 0;
+    this.list.forEach( function(value) {
+      somme += Number(value);
+    });
+  this.Initialiser(somme);
+  } 
+  Difference(){
+    //console.log()
+    let moins: number;
+    this.list.forEach( function(value, index, array) {
+      if (index === 0 ) {
+        moins = Number(value);
+        console.log(value);
+      }else{
+        console.log(value);
+        moins -= Number(value);
+      }
+    });
+    this.Initialiser(moins);
+  }
+  Produit(){
+    let fois: number = 1;
+    this.list.forEach( function(value) {
+      fois *= Number(value);
+    });
+    this.Initialiser(fois);
+  }
+  Divison(){
+    let div: number;
+    this.list.forEach( function(value, index, array) {
+      if (index === 0 ) {
+        div = Number(value);
+      }else{
+        if (value === 0) {
+          this.Calculate = 'Error';
+          this.valueInit = true;
+          this.list = [];
           this.Operator = '';
-          this.list.splice((this.list.length - 1));
+        } else {
+          div /= Number(value);
         }
       }
-      
+    });
+    this.Initialiser(div);
+  }
+ // La fonction pour efacer la derniere entree de l'ecran
+  Supprimer1(){
+     
+     if (this.Calculate.length === 1) {
+      this.Calculate = '0';
+      this.valueInit = true;
+    } else {
+      this.Calculate = this.Calculate.substring(0, (this.Calculate.length - 1));
+      if (isNumber(this.Calculate.substring( (this.Calculate.length - 1), this.Calculate.length))) {
+        this.readyForAnother = true;
+        this.Operator = '';
+        this.list.pop();
+      }
     }
+  }
 
-    // La virgule
-    else if (symbol === '.' ) {
-     if (this.readyForAnother) {
-       this.Calculate += '.';
-     }
-      }  
-     // L'egalite
-    else if (symbol === '=') {
-      console.log(!isNumber(this.Calculate));
-      if (!isNumber(this.Calculate)) { 
-           this.Calculer();
-      } 
-    }
-    else { 
-      this.signeCalcul(symbol);}
+  Modulo(){
+    let div: number;
+    this.list.forEach( function(value, index, array) {
+      if (index === 0 ) {
+        div = value;
+      }else{
+        if (value === 0) {
+         this.Erreur();
+        } else {
+          div %= value;
+        }
+      }
+    });
+   this.Initialiser(div); 
+  }
+  Initialiser(result:number){
+    this.Calculate = '' + result;
+    this.list = [];
+    this.Operator = '';
    
-}
- }
+  }
+  Erreur(){
+    this.Calculate = 'Error';
+          this.valueInit = true;
+          this.list = [];
+          this.Operator = '';
+  }
+  } 
